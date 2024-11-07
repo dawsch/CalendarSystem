8807fe6e640df217210018cfb6e98136b1d7019d
DawidSchneider <dawsch@st.amu.edu.pl>
2024-11-07 22:25:34 +0100

    inicjalizacja, wstępny szablon kalendarza

---
+++ calendar/calendar/App.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace calendar
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+
+}
---
+++ calendar/calendar/calendarControl.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ExceptionServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace calendar
+{
+    /// <summary>
+    /// Logika interakcji dla klasy calendarControl.xaml
+    /// </summary>
+    public partial class calendarControl : UserControl
+    {
+        DateTime actualData;
+        public calendarControl()
+        {
+            InitializeComponent();
+
+            actualData = DateTime.Now;
+            int firstDayOfMonty = ((int)new DateTime(actualData.Year, actualData.Month, 1).DayOfWeek) - 1;
+            int dayNumber = -firstDayOfMonty + 1;
+            int daysOfPrevMonth = DateTime.DaysInMonth(actualData.Year, actualData.Month - 1);
+
+            for (int i = 0; i < 6; i++)
+            {
+                
+                for (int j = 0; j < 7; j++)
+                {
+                    int offset = 0;
+                    if (dayNumber <= 0)
+                    {
+                        offset = daysOfPrevMonth;
+                    }
+                    if (dayNumber > DateTime.DaysInMonth(actualData.Year, actualData.Month))
+                    {
+                        offset = -DateTime.DaysInMonth(actualData.Year, actualData.Month);
+                    }
+                    customCalendarDayControl day = new customCalendarDayControl(dayNumber + offset);
+                    Grid.SetColumn(day, j);
+                    Grid.SetRow(day, i);
+                    DaysContainer.Children.Add(day);
+
+
+                    dayNumber++;
+                }
+            }
+        }
+    }
+}
